5441ae0bf115b321b5de456e7e6f5b53617e72a2
Ben Caterine <benrcaterine@gmail.com>
2022-01-21 14:06:10 -0600

    Committing my completed lab03 files

---
+++ lab03/answers.sql
@@ -0,0 +1,56 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name 
+FROM users;
+
+
+
+-- Exercise 3
+SELECT id, first_name, last_name FROM users ORDER BY last_name;
+
+
+
+-- Exercise 4
+SELECT id, user_id, image_url FROM posts WHERE user_id = 26;
+
+-- Exercise 5
+SELECT id, image_url, user_id FROM posts WHERE user_id = 26 OR user_id=12;
+
+-- Exercise 6
+SELECT count(*) FROM posts;
+
+-- Exercise 7
+SELECT user_id, count(*) FROM comments GROUP BY user_id ORDER BY count(*) desc;
+
+-- Exercise 8
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts INNER JOIN users ON posts.user_id = users.id
+WHERE posts.user_id = 12 OR posts.user_id = 26;
+
+-- Exercise 9
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts INNER JOIN following ON posts.user_id = following.following_id
+WHERE following.user_id = 26;
+
+-- Exercise 10
+SELECT posts.id, posts.pub_date, following.following_id, users.username
+FROM following
+INNER JOIN posts on posts.user_id = following.following_id
+INNER JOIN users on users.id = following.following_id
+WHERE following.user_id = 26
+ORDER BY posts.pub_date desc;
+
+-- Exercise 11
+
+
+-- Exercise 12
+
+
+-- Exercise 13
+
+
+-- Exercise 14
